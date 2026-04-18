77a5f5f40caf3a91bf8563b4d5b819a369b0843c
Omar Jabour <jabour.omar@hotmail.com>
2026-04-18 20:06:06 +0200

    working on services

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, HttpCode, Param, ParseIntPipe } from '@nestjs/common';
+import { 
+    Body,
+    Controller,
+    Delete,
+    Get,
+    HttpCode,
+    Param,
+    ParseIntPipe,
+    Patch,
+    Post 
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import type { User } from '../../types/usersType';
 
@@ -17,5 +27,23 @@ export class UsersController {
 
     findOne(@Param('id', ParseIntPipe) id: number): User {
         return this.usersService.findOne(id);
+    }
+
+    @Post()
+    @HttpCode(201)
+    create(@Body() user: User): User {
+    return this.usersService.create(user);
+    }
+    
+    @Patch(':id')
+    @HttpCode(200)
+    update(@Param('id', ParseIntPipe) id: number, @Body() user: User): User {
+        return this.usersService.update(id, user);
+    }
+
+    @Delete(':id')
+    @HttpCode(200)
+    delete(@Param('id', ParseIntPipe) id: number): string {
+        return this.usersService.delete(id);
     }
 }
