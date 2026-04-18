d5a2f52d55df56f5ff07f41759bad73e5efbe3a5
Omar Jabour <jabour.omar@hotmail.com>
2026-04-18 19:41:29 +0200

    working on services  .

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, ParseIntPipe } from '@nestjs/common';
+import { UsersService } from './users.service';
+import type { User } from '../../types/usersType';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Get()
+    @HttpCode(200)
+    findAll(): User[] {
+        return this.usersService.findAll();
+    }
+
+    @Get(':id')
+    @HttpCode(200)
+
+    findOne(@Param('id', ParseIntPipe) id: number): User {
+        return this.usersService.findOne(id);
+    }
+}
