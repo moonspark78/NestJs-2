fbfeaeeb2ad5a87ff90773bdd6c21e09348f9ca4
Omar Jabour <jabour.omar@hotmail.com>
2026-04-19 00:35:44 +0200

    working on dto ./.

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,22 @@
+import { 
+    IsEnum, 
+    IsNotEmpty, 
+    IsOptional, 
+    IsString, 
+    MaxLength, 
+    MinLength 
+} from "class-validator";
+
 export class CreateUserDto {
+    @IsString({ message: 'Name must be a string' })
+    @IsNotEmpty({ message: 'Name is required' })
+    @MinLength(3, { message: 'Name must be at least 3 characters long' })
+    @MaxLength(20, { message: 'Name must be at most 20 characters long' })
     name?: string;
+    @IsString({ message: 'Email must be a string' })
+    @IsNotEmpty({ message: 'Email is required' })
     email?: string
+    @IsOptional()
+    @IsEnum(['admin', 'user'], { message: 'Role must be either admin or user' })
     role?: 'admin' | 'user'; 
 }
