4eaf37c1cddf47112b9dfd6cd45ec2e94a783954
Omar Jabour <jabour.omar@hotmail.com>
2026-04-19 12:57:03 +0200

    working on dto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,6 +12,8 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import type { User } from '../../types/usersType';
+import type { CreateUserDto } from './dto/create-user.dto';
+import type { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -41,19 +43,18 @@ export class UsersController {
 
     @Post()
     @HttpCode(201)
-    create(@Body() user: User): User {
-    return this.usersService.create(user);
+    create(@Body() createUser: CreateUserDto): User {
+    return this.usersService.create(createUser);
     }
     
     @Patch(':id')
     @HttpCode(200)
-    update(@Param('id', ParseIntPipe) id: number, @Body() user: User): User {
-        return this.usersService.update(id, user);
+    update(@Param('id', ParseIntPipe) id: number, @Body() updateUser: UpdateUserDto): User {
+        return this.usersService.update(id, updateUser);
     }
 
     @Delete(':id')
-    @HttpCode(200)
-    delete(@Param('id', ParseIntPipe) id: number): string {
+    @HttpCode(200)    delete(@Param('id', ParseIntPipe) id: number): string {
         return this.usersService.delete(id);
     }
 }
