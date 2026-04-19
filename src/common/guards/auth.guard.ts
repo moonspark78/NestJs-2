4bb012bf1239a0bef5f6a9efe213fc6fa4392783
Omar Jabour <jabour.omar@hotmail.com>
2026-04-19 15:30:18 +0200

    working on dto ./.

--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -9,7 +9,7 @@ import { Request } from "express";
 @Injectable()
 export class AuthGuard implements CanActivate {
     canActivate(context: ExecutionContext): boolean {
-        const request = context.switchToHttp().getRequest()<Request>();
+        const request = context.switchToHttp().getRequest<Request>();
         const token = this.extractTokenFromHeader(request);
         if (!token) {
             throw new UnauthorizedException("No token provided");
